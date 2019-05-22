9eb7b852061d785e0ec8a0cc84027673fb3fd737
erfinfeluzy <erfin.feluzy@gmail.com>
2019-05-22 10:45:13 +0700

    baru

--- src/main/java/com/training/msa/config/rest/NotificationRestController.java
+++ src/main/java/com/training/msa/config/rest/NotificationRestController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.training.msa.config.jms.Email;
-import com.training.msa.config.jms.JmsSender;
+import com.training.msa.email.Email;
+import com.training.msa.email.JmsSender;
 
 @RestController
 public class NotificationRestController {
