4627745398e143453157556658e9263108987011
Erfin Feluzy <efeluzy@redhat.com>
2019-10-24 15:34:08 +0700

    refactor rest controller

--- src/main/java/com/training/msa/rest/NotificationRestController.java
+++ src/main/java/com/training/msa/rest/NotificationRestController.java
@@ -1,4 +1,4 @@
-package com.training.msa.config.rest;
+package com.training.msa.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
